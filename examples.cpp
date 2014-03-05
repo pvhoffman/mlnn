ddc486ec96044a3630cbde38724cdc3d17c68e6e
Paul V. Hoffman <pvhoffman@gmail.com>
2014-03-05 10:03:17 -0800

    added one bit full adder NN example

--- examples.cpp
+++ examples.cpp
@@ -1,6 +1,143 @@
 #include "stdafx.h"
 #include "neuralnet.h"
 
+/*
+One bit full adder truth table
+Inputs 	Outputs
+    A 	B 	Cin 	Cout 	S
+0:  0 	0 	0 	0 	0
+1:  1 	0 	0 	0 	1 OR  0 	1 	0 	0 	1
+2:  1 	1 	0 	1 	0
+3:  0 	0 	1 	0 	1
+4:  1 	0 	1 	1 	0 OR  0 	1 	1 	1 	0
+5:  1 	1 	1 	1 	1
+*/
+void ADDERNetworkExample()
+{
+    // one bit full adder network example
+    // 3 inputs: A + B & Carry bit In
+    // 2 outputs: S & Carry bit Out
+    // 6 possible outcomes to predict
+    mlnn::NNetwork::NNLayerDesc_t AdderDesc[] = { {3}, {6} };
+    mlnn::NNetwork AdderNetwork(AdderDesc, 2);
+    AdderNetwork.setLambda(0.0);
+
+    double J = 0.0;
+
+    // create some data with which to train
+    arma::mat X = arma::zeros(5000, 3);
+    arma::mat y = arma::zeros(5000, 1);
+
+    for( unsigned i = 0; i < 5000; i++){
+        unsigned a = ( rand() & 1 ) ? 1 : 0;
+        unsigned b = ( rand() & 1 ) ? 1 : 0;
+        unsigned c = ( a & b ) ? 1 : 0;
+        unsigned ys = 0;
+
+        if(!a && !b && !c){
+                ys = 0;
+        } else if( (a && !b && !c) || (!a && b && !c) ){
+                ys = 1;
+        } else if(a && b && !c){
+                ys = 2;
+        } else if(!a && !b && c){
+                ys = 3;
+        } else if( (a && !b && c) || (!a && b && c) ){
+                ys = 4;
+        } else if(a && b && c){
+                ys = 5;
+        } else {
+                std::cout << "Invalid condition in ADDER training data." << std::endl; 
+        }
+
+        X(i, 0) = a;
+        X(i, 1) = b;
+        X(i, 2) = c;
+        y(i)    = ys;
+    }
+
+    // 1000 iterations might do
+    for(unsigned i = 0; i< 1000; i++){
+        J = AdderNetwork.train(X,y);
+        if( !(i % 100) ){
+            std::cout << "Current cost for ADDER network at training iteration " << i << " is " << J << std::endl;
+        }
+    }
+
+    std::cout << "Final cost for ADDER network at end training is " << J << std::endl;
+
+    // recreate training data
+    for( unsigned i = 0; i < 5000; i++){
+        unsigned a = ( rand() & 1 ) ? 1 : 0;
+        unsigned b = ( rand() & 1 ) ? 1 : 0;
+        unsigned c = ( a & b ) ? 1 : 0;
+        unsigned ys = 0;
+
+        if(!a && !b && !c){
+                ys = 0;
+        } else if( (a && !b && !c) || (!a && b && !c) ){
+                ys = 1;
+        } else if(a && b && !c){
+                ys = 2;
+        } else if(!a && !b && c){
+                ys = 3;
+        } else if( (a && !b && c) || (!a && b && c) ){
+                ys = 4;
+        } else if(a && b && c){
+                ys = 5;
+        } else {
+                std::cout << "Invalid condition in ADDER training data." << std::endl; 
+        }
+
+        X(i, 0) = a;
+        X(i, 1) = b;
+        X(i, 2) = c;
+        y(i)    = ys;
+    }
+
+    unsigned badp = 0;
+
+
+    // check the predictions
+    for( unsigned i = 0; i < 5000; i++){
+        arma::mat px = X.row(i);
+
+        unsigned p = AdderNetwork.predict(px);
+
+        unsigned a = (unsigned)X(i,0); 
+        unsigned b = (unsigned)X(i,1);
+        unsigned c = (unsigned)X(i,2);
+        unsigned d = 0;
+
+        if(!a && !b && !c){
+                d = 0;
+        } else if( (a && !b && !c) || (!a && b && !c) ){
+                d = 1;
+        } else if(a && b && !c){
+                d = 2;
+        } else if(!a && !b && c){
+                d = 3;
+        } else if( (a && !b && c) || (!a && b && c) ){
+                d = 4;
+        } else if(a && b && c){
+                d = 5;
+        } else {
+                std::cout << "Invalid condition in ADDER training data." << std::endl; 
+        }
+
+
+
+        if(p != d){
+                std::cout << "Incorrect prediction at " << i << ". A = " << p << ", P = " << d << std::endl;
+                badp++;
+        }
+
+    }
+    
+    std::cout << badp << " incorrect predictions from trained ADDER network." << std::endl << std::endl;
+
+
+}
 // create a neural network to predict the result
 // of an AND gate with two binary inputs and 1 binary output
 void ANDNetworkExample()
@@ -13,7 +150,8 @@ void ANDNetworkExample()
     AndNetwork.setLambda(0.0);
 
 
-    double J = 0;
+    double J = 0.0;
+    double K = 0.0;
 
     // create some data with which to train
     arma::mat X = arma::zeros(5000, 2);
@@ -35,6 +173,10 @@ void ANDNetworkExample()
         if( !(i % 100) ){
             std::cout << "Current cost for AND network at training iteration " << i << " is " << J << std::endl;
         }
+        //TEST_CASE("ANDNetworkExample/training", "Test the cost of the network is descending"){
+        //        REQUIRE( K <= J);
+        //}
+        //K = J;
     }
 
     std::cout << "Final cost for AND network at end training is " << J << std::endl;
@@ -60,9 +202,13 @@ void ANDNetworkExample()
         unsigned a = (unsigned)X(i,0) & (unsigned)X(i,1);
 
         if(p != a){
-                std::cout << "Incorrect prediction at " << i << ". P is " << p << " and A is " << a << "." << std::endl;
+                std::cout << "Incorrect prediction at " << i << ". A = " << p << ", P = " << p << std::endl;
                 badp++;
         }
+
+        //TEST_CASE("ANDNetworkExample/predicting", "Test the network is predicting correct values"){
+        //        REQUIRE( p == a);
+        //}
 
     }
     
@@ -214,5 +360,6 @@ int main (int argc, char const* argv[])
     ANDNetworkExample();
     ORNetworkExample();
     XORNetworkExample();
+    ADDERNetworkExample();
     return 0;
 }
